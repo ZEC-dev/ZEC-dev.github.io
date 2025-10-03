e2dae614c529d7d18e2a84336130a70a20941396
ZEC-dev <13100616336@163.com>
2025-10-03 16:22:11 +0800

    Create First.js

---
+++ tampermonkey/First.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         ZEC
+// @namespace    https://zec-dev.github.io/
+// @version      1.0.0
+// @description  人机验证
+// @author       ZEC-dev
+// @homepage     https://zec-dev.github.io/
+// @icon         https://zec-dev.github.io/img/ico.ico
+// @license      MIT
+
+// @match        http://*/*
+// @match        https://*/*
+// @updateURL    https://zec-dev.github.io/tampermonkey/First.js
+// @downloadURL  https://zec-dev.github.io/tampermonkey/First.js
+// @supportURL   https://zec-dev.github.io/tampermonkey/
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert('人机验证!');
+})();
